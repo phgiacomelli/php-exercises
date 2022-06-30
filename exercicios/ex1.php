dbbb57aaa6ac134faf4060545ca08a655418fce7
phgiacomelli <101350035+phgiacomelli@users.noreply.github.com>
2022-06-30 10:00:13 -0300

    udapte

--- exercicios/ex1.php
+++ exercicios/ex1.php
@@ -23,24 +23,34 @@
 <body>
    <table>
       <?php
+         //adicionando linha na tabela
          $tabela.= '<tr>';
+
+         //adicionando o * como primeiro elemento
          $tabela.= '<td>*</td>';
 
+         // adicionando a linha de 0 a 9
          for ($i=0; $i < 10; $i++) { 
             $tabela .= '<td>'.$i.'</td>' ;
          };
 
+         // fechando tag da linha
          $tabela.= '</tr>';
 
+         // for que adiciona a coluna de 0 a 9
          for ($i=0; $i < 10; $i++) { 
-
+            //criando nova linha e elemento (de 0 a 9)
             $tabela.= '<tr><td>' . $i;
+
+            //for que adiciona a tabuada do numero referente a linha "i"
             for ($j=0; $j < 10; $j++) { 
                $result = $j*$i;
                $tabela .= '<td>'.$result.'</td>';
             };
+            // fechando tag do elemento e linha
             $tabela.='</td></tr>';
          };
+         //printa a tabela
          echo $tabela;
       ?>
    </table>
